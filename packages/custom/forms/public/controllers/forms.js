c0075c44fa49242499b18936a50ec8666c3cb628
Kevin Li <kevin.li@safetyauthority.ca>
2016-01-20 15:52:48 -0800

    Add readonly feature

--- packages/custom/forms/public/controllers/forms.js
+++ packages/custom/forms/public/controllers/forms.js
@@ -121,104 +121,111 @@ angular.module('mean.forms').config(function(formlyConfigProvider) {
     });
 
     formlyConfigProvider.setType({
-      name: 'bcsa_multiCheckbox',
-      templateUrl: 'bcsa_multiCheckbox.html',
-      wrapper: ['bootstrapLabel', 'bootstrapHasError'],
-      apiCheck: check => ({
-        templateOptions: {
-          options: check.arrayOf(check.object),
-          labelProp: check.string.optional,
-          valueProp: check.string.optional
-        }
-      }),
-      defaultOptions: {
-        noFormControl: false,
-        ngModelAttrs: {
-          required: {
-            attribute: '',
-            bound: ''
-          }
-        }
-      },
-      controller: /* @ngInject */ function($scope) {
-        const to = $scope.to;
-        const opts = $scope.options;
-        $scope.multiCheckbox = {
-          checked: [],
-          change: setModel
-        };
-
-        // initialize the checkboxes check property
-        $scope.$watch('model', function modelWatcher(newModelValue) {
-          var modelValue, valueProp;
-
-          if (Object.keys(newModelValue).length) {
-            modelValue = newModelValue[opts.key];
-
-            $scope.$watch('to.options', function optionsWatcher(newOptionsValues) {
-              if (newOptionsValues && Array.isArray(newOptionsValues) && Array.isArray(modelValue)) {
-                valueProp = to.valueProp || 'value';
-                for (var index = 0; index < newOptionsValues.length; index++) {
-                  $scope.multiCheckbox.checked[index] = modelValue.indexOf(newOptionsValues[index][valueProp]) !== -1;
+        name: 'bcsa_multiCheckbox',
+        templateUrl: 'bcsa_multiCheckbox.html',
+        wrapper: ['bootstrapLabel', 'bootstrapHasError'],
+        apiCheck: check => ({
+            templateOptions: {
+                options: check.arrayOf(check.object),
+                labelProp: check.string.optional,
+                valueProp: check.string.optional
+            }
+        }),
+        defaultOptions: {
+            noFormControl: false,
+            ngModelAttrs: {
+                required: {
+                    attribute: '',
+                    bound: ''
                 }
-              }
-            });
-          }
-        }, true);
-
-        function checkValidity(expressionValue) {
-          var valid;
-
-          if ($scope.to.required) {
-            valid = angular.isArray($scope.model[opts.key]) &&
-              $scope.model[opts.key].length > 0 &&
-              expressionValue;
-
-            $scope.fc.$setValidity('required', valid);
-          }
-        }
-
-        function setModel() {
-          $scope.model[opts.key] = [];
-          angular.forEach($scope.multiCheckbox.checked, (checkbox, index) => {
-            if (checkbox) {
-              $scope.model[opts.key].push(to.options[index][to.valueProp || 'value']);
             }
-          });
+        },
+        controller: /* @ngInject */ function($scope) {
+            const to = $scope.to;
+            const opts = $scope.options;
+            $scope.multiCheckbox = {
+                checked: [],
+                change: setModel
+            };
 
-          // Must make sure we mark as touched because only the last checkbox due to a bug in angular.
-          $scope.fc.$setTouched();
-          checkValidity(true);
-        }
+            // initialize the checkboxes check property
+            $scope.$watch('model', function modelWatcher(newModelValue) {
+                var modelValue, valueProp;
 
-        if (opts.expressionProperties && opts.expressionProperties['templateOptions.required']) {
-          $scope.$watch(function() {
-            return $scope.to.required;
-          }, function(newValue) {
-            checkValidity(newValue);
-          });
-        }
+                if (Object.keys(newModelValue).length) {
+                    modelValue = newModelValue[opts.key];
 
-        if ($scope.to.required) {
-          var unwatchFormControl = $scope.$watch('fc', function(newValue) {
-            if (!newValue) {
-              return;
+                    $scope.$watch('to.options', function optionsWatcher(newOptionsValues) {
+                        if (newOptionsValues && Array.isArray(newOptionsValues) && Array.isArray(modelValue)) {
+                            valueProp = to.valueProp || 'value';
+                            for (var index = 0; index < newOptionsValues.length; index++) {
+                                $scope.multiCheckbox.checked[index] = modelValue.indexOf(newOptionsValues[index][valueProp]) !== -1;
+                            }
+                        }
+                    });
+                }
+            }, true);
+
+            function checkValidity(expressionValue) {
+                var valid;
+
+                if ($scope.to.required) {
+                    valid = angular.isArray($scope.model[opts.key]) &&
+                        $scope.model[opts.key].length > 0 &&
+                        expressionValue;
+
+                    $scope.fc.$setValidity('required', valid);
+                }
             }
-            checkValidity(true);
-            unwatchFormControl();
-          });
+
+            function setModel() {
+                $scope.model[opts.key] = [];
+                angular.forEach($scope.multiCheckbox.checked, (checkbox, index) => {
+                    if (checkbox) {
+                        $scope.model[opts.key].push(to.options[index][to.valueProp || 'value']);
+                    }
+                });
+
+                // Must make sure we mark as touched because only the last checkbox due to a bug in angular.
+                $scope.fc.$setTouched();
+                checkValidity(true);
+            }
+
+            if (opts.expressionProperties && opts.expressionProperties['templateOptions.required']) {
+                $scope.$watch(function() {
+                    return $scope.to.required;
+                }, function(newValue) {
+                    checkValidity(newValue);
+                });
+            }
+
+            if ($scope.to.required) {
+                var unwatchFormControl = $scope.$watch('fc', function(newValue) {
+                    if (!newValue) {
+                        return;
+                    }
+                    checkValidity(true);
+                    unwatchFormControl();
+                });
+            }
         }
-      }
     });
 
     formlyConfigProvider.setType({
-      name: 'bcsa_checkbox',
-      templateUrl: 'bcsa_checkbox.html',
-      wrapper: ['bootstrapHasError'],
-      apiCheck: check => ({
-        templateOptions: {
-            label: check.string
-        }
-      })
+        name: 'bcsa_checkbox',
+        templateUrl: 'bcsa_checkbox.html',
+        wrapper: ['bootstrapHasError'],
+        apiCheck: check => ({
+            templateOptions: {
+                label: check.string
+            }
+        })
+    });
+
+    formlyConfigProvider.setType({
+        name: 'input',
+        templateUrl: 'input-template.html',
+        wrapper: ['bootstrapLabel', 'bootstrapHasError'],
+        overwriteOk: true
     });
 });
