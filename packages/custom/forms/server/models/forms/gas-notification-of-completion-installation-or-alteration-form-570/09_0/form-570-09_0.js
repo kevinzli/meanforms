47ef5067979e970a1c134273daf3f1536c8e0d10
Jim Curran <jimcee@gmail.com>
2016-01-22 15:49:39 -0800

    Form 570, moved Type of Request section to first step, integrated Install Permit fields

--- packages/custom/forms/server/models/forms/gas-notification-of-completion-installation-or-alteration-form-570/09_0/form-570-09_0.js
+++ packages/custom/forms/server/models/forms/gas-notification-of-completion-installation-or-alteration-form-570/09_0/form-570-09_0.js
@@ -8,9 +8,32 @@ var formSchema={
             name: 'step0',
             formName: 'vm.forms.step0',
             title: 'START',
-            header: '',
+            header: 'A. TYPE OF REQUEST',
             buttonText: 'Continue',
             fields: [
+                {
+                    key: 'inspectionTypeRequested',
+                    type: 'bcsa_radio',
+                    templateOptions: {
+                        label: 'Inspection type requested',
+                        options: [
+                            {id: "commissioningApproval", title: "Commissioning approval*"},
+                            {id: "productApproval", title: "Product approval*"},
+                            {id: "roughIn", title: "Rough-in"},
+                            {id: "finalCompletion", title: "Final completion"},
+                            {id: "reinspectionNon-compliancesCorrected", title: "Re-inspection non-compliances corrected"},
+                        ],
+                        valueProp: 'id',
+                        labelProp: 'title',
+                        required: true
+                    },
+                    expressionProperties: {
+                        "templateOptions.disabled": "formState.disabled"
+                    }
+                },
+                {
+                    template: '<div class="alert alert-info">If you are submitting for commissioning approval or product approval, please also provide the Installation Permit Number.</div>'
+                },
                 {
                     key: 'installationPermitNumber',
                     type: 'input',
@@ -43,9 +66,6 @@ var formSchema={
                     expressionProperties: {
                         "templateOptions.disabled": "formState.disabled"
                     }
-                },
-                {
-                    template: '<div><p>* If your are submitting for commissioning approval or product approval, please also provide Installation Permit Number above.</p></div>'
                 }
             ]
         },
@@ -53,7 +73,7 @@ var formSchema={
             name: 'step1',
             formName: 'vm.forms.step1',
             title: '',
-            header: 'A. SITE INFORMATION',
+            header: 'B. SITE INFORMATION',
             buttonText: 'Continue',
             fields: [
                 {
@@ -104,7 +124,7 @@ var formSchema={
             name: 'step2',
             formName: 'vm.forms.step2',
             title: '',
-            header: 'B. PERMIT HOLDER DETAILS',
+            header: 'C. PERMIT HOLDER DETAILS',
             buttonText: 'Continue',
             fields: [
                 {
@@ -154,45 +174,13 @@ var formSchema={
         {
             name: 'step3',
             formName: 'vm.forms.step3',
-            title: '',
-            header: 'C. TYPE OF REQUEST',
-            buttonText: 'Continue',
-            fields: [
-                {
-                    key: 'inspectionTypeRequested',
-                    type: 'bcsa_multiCheckbox',
-                    templateOptions: {
-                        label: 'Inspection type requested',
-                        options: [
-                            {id: "commissioningApproval", title: "Commissioning approval*"},
-                            {id: "productApproval", title: "Product approval*"},
-                            {id: "roughIn", title: "Rough-in"},
-                            {id: "finalCompletion", title: "Final completion"},
-                            {id: "reinspectionNon-compliancesCorrected", title: "Re-inspection non-compliances corrected"},
-                        ],
-                        valueProp: 'id',
-                        labelProp: 'title',
-                        required: true
-                    },
-                    expressionProperties: {
-                        "templateOptions.disabled": "formState.disabled"
-                    }
-                },
-                {
-                    template: '<div><p>* If your are submitting for commissioning approval or product approval, please also provide Installation Permit Number above.</p></div>'
-                }
-            ]
-        },
-        {
-            name: 'step4',
-            formName: 'vm.forms.step4',
             title: 'FINISH',
             header: 'D. DECLARATION',
             buttonText: 'Submit',
             fields: [
                 {
                     key: 'signature',
-                    template: '<p>I, <input class="formly-field-input" ng-model="model[options.key]">, a gas fitter for the above listed licenced contractor or a homeowner, hereby declare that the gas installation authorized under above-mentioned permit number has been installed to comply with the Safety Standards Act and Regulations of British Columbia. </p>',
+                    template: '<p>I, <input class="formly-field-input" ng-model="model[options.key]">, a gas fitter for the above listed licensed contractor or homeowner, hereby declare that the gas installation authorized under above-mentioned permit number has been installed to comply with the Safety Standards Act and Regulations of British Columbia. </p>',
                     templateOptions: {
                         label: 'Gas fitter or homeowner name',
                         required: true,
@@ -203,15 +191,16 @@ var formSchema={
                 },
                 {
                     key: 'dateSigned',
-                    template: '<p> Placed in service (please provide date mm/dd/yyyy): <input class="formly-field-input" ng-model="model[options.key]"></p>',
+                    type: 'input',
+                    // template: '<p> Placed in service (please provide date mm/dd/yyyy): <input class="formly-field-input" ng-model="model[options.key]"></p>',
                     templateOptions: {
-                        label: 'Placed in service (please provide date)',
+                        label: 'Date placed in service (MMDDYYYY)',
                         required: true,
                     },
                     expressionProperties: {
                         "templateOptions.disabled": "formState.disabled"
                     }
-                },
+                },                
                 {
                     key: 'declaration',
                     type: 'bcsa_checkbox',
