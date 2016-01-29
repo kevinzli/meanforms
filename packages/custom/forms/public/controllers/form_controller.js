46e1218c3eea19a6e166d1f0437bb9e1217622f7
Kevin Li <kevin.li@safetyauthority.ca>
2016-01-29 10:36:37 -0800

    Add ngPrint directive

--- packages/custom/forms/public/controllers/form_controller.js
+++ packages/custom/forms/public/controllers/form_controller.js
@@ -40,7 +40,7 @@ angular.module('mean.forms').controller('FormController', ['$scope', 'Global', '
                 generateFormContent(form, formName, formSchema),
                 'Please save the link below if you need to visit this page later:',
                 //$location.protocol() + '://' +$location.host() + ':' + $location.port() + '/forms/' + form._id +'/confirmation',
-                '<span style="word-break: break-all">$location.absUrl()</span>',
+                '<span style="word-break: break-all">' + $location.absUrl() +'</span>',
                 '<br />'
             ].join('<br />');
 
@@ -148,3 +148,44 @@ angular.module('mean.forms').controller('FormController', ['$scope', 'Global', '
 
     }
 ]);
+
+function printDirective() {
+    var printSection = document.getElementById('printSection');
+
+    // if there is no printing section, create one
+    if (!printSection) {
+        printSection = document.createElement('div');
+        printSection.id = 'printSection';
+        document.body.appendChild(printSection);
+    }
+
+    function link(scope, element, attrs) {
+        element.on('click', function() {
+            var elemToPrint = document.getElementById(attrs.printElementId);
+            if (elemToPrint) {
+                printElement(elemToPrint);
+                window.print();
+            }
+        });
+
+        window.onafterprint = function() {
+            // clean the print section before adding new content
+            printSection.innerHTML = '';
+        }
+    }
+
+    function printElement(elem) {
+        // clones the element you want to print
+        var domClone = elem.cloneNode(true);
+        printSection.appendChild(domClone);
+    }
+
+    return {
+        link: link,
+        restrict: 'A'
+    };
+}
+
+angular
+    .module('mean.forms')
+    .directive('ngPrint', [printDirective]);
